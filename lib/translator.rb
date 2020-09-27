bda690ed9baa7f60686332c920d6250ebbbf0d56
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-27 07:23:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
+require 'pry'
+require "yaml"
 # require modules here
 
-def load_library
+def load_library path
   # code goes here
+  emoticon_temp = YAML.load_file(path)
+  emoticon_library = emoticon_temp.each_with_object({}) do |(key,value),library|
+    library[key]={ :english => value[0], :japanese => value[1] }
+    
+ # binding.pry
+  end
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
+  library = load_library path
+  retval = library.select {|key, value|  
+    if value[:english] == emoticon
+      value[:japanese]
+    end}
+  retval
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
+  library = load_library path
+  retval = library.select {|key, value|  
+    if value[:japanese] == emoticon
+      key
+    end
+    retval
+    
+  #binding.pry
+  }
+
   # code goes here
 end
